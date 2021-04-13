b86d7a082c1be79e2034cb79014c44e404ae1780
Hussein Mohamed <hussein.abdallah96@eng-st.cu.edu.eg>
2021-04-13 11:31:34 +0200

    Improve comments

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
-// setup handlebars engine and views location
+// setup handlebars engine and views location and partials location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
